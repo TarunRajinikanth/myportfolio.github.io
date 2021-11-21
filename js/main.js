b69542aa6c661fb7b8076c8c3aef4be9b42ea77b
TarunRajinikanth <tarunpatnala@gmail.com>
2021-11-21 23:24:15 +0530

    Upgrade 2.1.0

--- js/main.js
+++ js/main.js
@@ -176,3 +176,15 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+var nextStep = document.querySelector('#nextStep');
+
+nextStep.addEventListener('click', function(e) {
+    e.preventDefault();
+    // Hide first view
+    document.getElementById('my_form').style.display = 'none';
+
+    // Show thank you message element
+    document.getElementById('thank_you').style.display = 'block';
+});
